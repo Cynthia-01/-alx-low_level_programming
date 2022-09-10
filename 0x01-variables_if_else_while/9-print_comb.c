08dba3572fdba8fadbaeb38a86ed9743f1d4aa38
Cynthia-01 <111015595+Cynthia-01@users.noreply.github.com>
2022-09-10 02:43:12 +0100

    print comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,49 +1,23 @@
 #include <stdio.h>
-
-#include <stdlib.h>
-
-#include <time.h>
-
 /**
+ * main - entry point
  *
- *  * main - Entry point
+ * Return: alway returns 0
  *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-
+ */
 int main(void)
-
 {
+	int i;
 
-		int d;
-
-
-
-			for (d = '0'; d <= '9'; d++)
-
-					{
-
-								putchar(d);
-
-										if (d != '9')
-
-													{
-
-																	putchar(',');
-
-																				putchar(' ');
-
-																						}
-
-											}
-
-				putchar('\n');
-
-
-
-					return (0);
-
+	for (i = 10; i < 20; i++)
+	{
+		putchar((i % 10) + '0');
+		if (i != 19)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
 }
