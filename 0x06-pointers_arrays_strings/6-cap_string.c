e5dc1464bb250e4b7a11a1071eec2ef465a9ba79
Cynthia-01 <111015595+Cynthia-01@users.noreply.github.com>
2022-09-22 18:17:56 +0100

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,57 +1,44 @@
 #include "main.h"
 
 /**
- * seperator - checks if character is a seperator
- * @c: character to be checked
- * Return: if seperator return 1. Otherwise return 0;
- */
-
-int separator(char c)
-{
-	/*Delcaring condition switch*/
-	switch (c)
-	{
-		case ' ':
-		case '\t':
-		case '\n':
-		case ',':
-		case ';':
-		case '.':
-		case '!':
-		case '?':
-		case '"':
-		case '(':
-		case ')':
-		case '{':
-		case '}':
-			return (1);
-
-		default:
-			return (0);
-	}
-}
-/**
- * cap_string - capitalizes chars after given deliminators
- * @s: string to uppercase
- * Return: returns modified string
+ *cap_string - capitalizes every first letter of a word in a string.
+ *separators of words are:  space, tabulation,
+ * new line, ,, ;, ., !, ?, ", (, ), {, and }.
+ *@s: pointer to string.
+ *
+ *Return: pointer to s.
  */
 char *cap_string(char *s)
 {
-	int count, upper;
+	int count;
 
-	upper = -32; /*value constant 32*/
+/*  scan through string */
+
 	count = 0;
-	/*Start WHILE*/
+
 	while (s[count] != '\0')
-	{
-		/*letters lowercase*/
-		if (s[count] >= 'a' && s[count] <= 'z')
+
+	{/* if next character after count is a char , capitalise it */
+
+		if (s[0] >= 97 && s[0] <= 122)
 		{
-			/*Convert uppercase*/
-			if (s[count] == *s || separator(s[count - 1]))
-				s[count] += upper;
+			s[0] = s[0] - 32;
 		}
-		count++; /*Add count*/
+
+		if (s[count] == ' ' || s[count] == '\t' || s[count] == '\n'
+		    || s[count] == ',' || s[count] == ';' || s[count] == '.'
+		    || s[count] == '.' || s[count] == '!' || s[count] == '?'
+		    || s[count] == '"' || s[count] == '(' || s[count] == ')'
+		    || s[count] == '{' || s[count] == '}')
+
+		{
+			if (s[count + 1] >= 97 && s[count + 1] <= 122)
+			{
+				s[count + 1] = s[count + 1] - 32;
+			}
+		}
+		count++;
 	}
+
 	return (s);
 }
