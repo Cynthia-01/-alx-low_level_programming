9c3bf8331af0d68f9b45aa1b3229d4cad0fce9cc
Cynthia-01 <111015595+Cynthia-01@users.noreply.github.com>
2022-09-28 15:26:18 +0100

    Update 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,59 +1,21 @@
 #include "main.h"
 
-
-
 /**
-
- * _sqrt_recursion - main funct
-
- * @n: int n
-
- * Return: int
-
+ * _pow_recursion - pow recursion
+ * @x: integer params
+ * @y: integer paramtr
+ * Return: recursion
  */
 
-
-
-int _sqrt_recursion(int n)
-
+int _pow_recursion(int x, int y)
 {
-
-	return (_sqrt(n, 1));
-
-}
-
-
-
-/**
-
- * _sqrt - _sqrt_recursion
-
- * @n: integer paramtr
-
- * @i: integer parameter
-
- * Return: sqrt
-
- */
-
-
-
-int _sqrt(int n, int i)
-
-{
-
-	if (n < 0)
-
+	if (y < 0)
+	{
 		return (-1);
-
-	if ((i * i) > n)
-
-		return (-1);
-
-	if (i * i == n)
-
-		return (i);
-
-	return (_sqrt(n, i + 1));
-
+	}
+	if (y == 0)
+	{
+		return (1);
+	}
+	return (x * _pow_recursion(x, y - 1));
 }
