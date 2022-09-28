222cfdece950ac525c5e5b945199fa1c877f7313
Cynthia-01 <111015595+Cynthia-01@users.noreply.github.com>
2022-09-28 15:14:34 +0100

    Update 2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,10 +1,5 @@
 #include "main.h"
 
-
-
-/**
-#include "main.h"
-
 /**
  * _strlen_recursion - size
  * @s: pointer to string params
@@ -18,29 +13,4 @@ int _strlen_recursion(char *s)
 		return (0);
 	}
 	return (1 + _strlen_recursion(++s));
-}
- * _strlen_recursion - size
-
- * @s: pointer to string params
-
- * Return: recursion
-
- */
-
-
-
-int _strlen_recursion(char *s)
-
-{
-
-	if (!*s)
-
-	{
-
-		return (0);
-
-	}
-
-	return (1 + _strlen_recursion(++s));
-
 }
